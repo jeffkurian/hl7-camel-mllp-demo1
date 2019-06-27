f5e01e483d5768f2e54f8496d891c0c8b1184244
jeffkurian <Summer2018!>
2019-06-27 18:21:37 -0400

    added log messages and Exception handler

--- src/main/java/com/example/demo/MyMllpRouter.java
+++ src/main/java/com/example/demo/MyMllpRouter.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import ca.uhn.hl7v2.AcknowledgmentCode;
+import ca.uhn.hl7v2.ErrorCode;
+import ca.uhn.hl7v2.HL7Exception;
 import org.apache.camel.builder.RouteBuilder;
 
 import org.apache.camel.component.hl7.HL7DataFormat;
@@ -8,6 +10,7 @@ import org.apache.camel.spi.DataFormat;
 import org.springframework.stereotype.Component;
 
 import static org.apache.camel.component.hl7.HL7.ack;
+import static org.apache.camel.component.hl7.HL7.messageConforms;
 
 /**
  * A simple Camel route that triggers from a timer and calls a bean and prints to system out.
@@ -21,11 +24,24 @@ public class MyMllpRouter extends RouteBuilder {
     public void configure() throws Exception {
         DataFormat hl7 = new HL7DataFormat();
 
+        onException(HL7Exception.class)
+                .log("Handling Exception ")
+                .transform(ack(AcknowledgmentCode.AE,"Error tranforming he message", ErrorCode.UNSUPPORTED_MESSAGE_TYPE))
+                .handled(true)
+                .end()  ;
+
         from("mllp://8088")
+                .log("Recieved message ")
                 .unmarshal(hl7)
+                .log("done unmarshelling")
+                .validate(messageConforms())
+                .log("done validating")
                 .bean(MyHL7BusinessLogicService.class,"handleA01")
+                .log("done calling business")
                 .transform(ack(AcknowledgmentCode.AA))
                 .end();
+
+
     }
 
 }
